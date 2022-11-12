f04276616d9978357d76a3033b62bbeb67566c19
FujiwaraKen <fjwr1024@gmail.com>
2022-11-12 18:57:33 +0900

    buildの邪魔になる箇所をコメントアウト

--- src/app/serverComponent/page.tsx
+++ src/app/serverComponent/page.tsx
@@ -1,23 +1,23 @@
 import { use } from 'react'
 
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/test')
-  return res.json()
-}
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/test')
+//   return res.json()
+// }
 
 type Test = {
   content: string
 }
 
 export default function Page() {
-  const tests: Test[] = use(getData())
+  // const tests: Test[] = use(getData())
 
   return (
     <>
       <h1>ServerComponentTest</h1>
-      {tests.map((test, i) => {
+      {/* {tests.map((test, i) => {
         return <div key={i}>{test.content}</div>
-      })}
+      })} */}
     </>
   )
 }
