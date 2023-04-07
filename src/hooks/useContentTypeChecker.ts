bb27d1e0ac134a3237a602263fbecd9a38ae7467
FujiwaraKen <fjwr1024@gmail.com>
2023-04-07 23:54:48 +0900

    content type checker add

---
+++ src/hooks/useContentTypeChecker.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export const useContentTypeChecker = (imgSrc: string) => {
+  const [contentType, setContentType] = useState<string>('');
+
+  useEffect(() => {
+    const checkContentType = async () => {
+      try {
+        const response = await fetch(imgSrc);
+        const resHeader = response.headers.get('Content-Type') as string;
+        setContentType(resHeader);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    checkContentType();
+  }, [imgSrc]);
+
+  const isImage = contentType.startsWith('image/');
+  const isVideo = contentType.startsWith('video/');
+
+  return {
+    contentType,
+    isImage,
+    isVideo,
+  };
+};
